b48866c67774c20efa8d141ff2a75b80b24d910d
GabrielOchieng <ogingagabriel@gmail.com>
2024-09-02 11:07:07 +0300

    hidden overflow for homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ import { Fade } from "react-awesome-reveal";
 
 const HomePage = () => {
   return (
-    <div className="bg-gray-50">
+    <div className="bg-gray-50 overflow-hidden">
       <Header />
       <Explore />
 
